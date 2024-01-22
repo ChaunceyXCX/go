9f80e134e9f7f6a3c1022b92b2555b37a3787b2b
chaunceyxie <chaunceyxcx@gmail.com>
2024-01-22 18:11:41 +0800

    update

---
+++ ch3/comma_float.go
@@ -0,0 +1,24 @@
+/*
+ * @Date: 2024-01-22 16:30:36
+ * @LastEditors: chaunceyxie chaunceyxcx@gmail.com
+ * @LastEditTime: 2024-01-22 17:07:30
+ * @FilePath: \go_learn\ch3\comma_float.go
+ * @Description:
+ */
+package comma
+
+import "strings"
+
+func Comma_float(s string) string {
+	// 分别处理小数点前后的
+	// 小数点前面的
+	before := s[:strings.LastIndex(s, ".")]
+	// 小数点后面的
+	after := s[strings.LastIndex(s, ".")+1:]
+	return Comma(before) + "." + Comma(after)
+}
+
+func commaAfter(s string) string{
+
+	return s[]
+}
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
+/*
+ * @Date: 2024-01-10 15:17:54
+ * @LastEditors: chaunceyxie chaunceyxcx@gmail.com
+ * @LastEditTime: 2024-01-22 17:04:41
+ * @FilePath: \go_learn\main.go
+ * @Description:
+ */
 package main
 
 import (
 	"fmt"
-	"go_learn/ch2/popcount"
+	comma "go_learn/ch3"
 )
 
 func main() {
-	fmt.Println(popcount.PopCount(10))
+	fmt.Println(comma.Comma_float("1234567890.9876543211"))	
 }
