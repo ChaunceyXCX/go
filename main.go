f29c030c0aaf874823f188e37f7da4f484820ada
chauncey <chaunceyxcx@gmail.com>
2024-07-28 20:07:34 +0800

    常量与变量

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 /*
  * @Date: 2024-01-10 15:17:54
- * @LastEditors: chaunceyxie chaunceyxcx@gmail.com
- * @LastEditTime: 2024-01-22 17:04:41
+ * @LastEditors: chauncey chaunceyxcx@gmail.com
+ * @LastEditTime: 2024-07-28 19:50:35
  * @FilePath: \go_learn\main.go
  * @Description:
  */
 package main
 
-import (
-	"fmt"
-	comma "go_learn/ch3"
-)
 
 func main() {
-	fmt.Println(comma.Comma_float("1234567890.9876543211"))	
+		
 }
